91810074c4c97baa0480b6a339dc5d2665ad8767
guipateiro <guilhermepateiro@gmail.com>
2023-02-04 16:44:18 -0300

    sim

--- src/Post.hpp
+++ src/Post.hpp
@@ -2,7 +2,12 @@
 #define POST_HPP
 
 #include<string>
+#include<vector>
 #include"Id.hpp"
+#include"Comentario.hpp"
+#include"Usuario.hpp"
+#include"Permisao.hpp"
+#include"Data.hpp"
 
 class Post{
     public:
@@ -12,16 +17,29 @@ class Post{
 		
 		virtual void run() const = 0;
 		std::string getLink() const;
+
+		void setTitulo(const std::string titulo);
+		std::string getTitulo() const;
+
+		void setDescrisao(const std::string descrisao);
+		std::string getDescrisao() const;
+
+		unsigned int getId() const;
+
+		void setPermissao(Permissao permissao);
+		Permissao getPermissao() const;
+
     private:
-		std::string link; 
-		std::string titulo;
-        std::string descrissao;
+		std::string link;  // nome do arquivo
+		std::string titulo; //titulo do post
+        std::string descrissao; // descrissao do post
 		unsigned int id; //ID unico q é compartilhado entre dos os posts
-		//Usuario user;
-		//std::list<usuarios> listaPermissoes;
-		//classe enum (PRIVATE, PUBLIC) //prafacilitar o teste por outras classes e pq da nota
-		//std::list<comentario> comentarios;
-		//std::list<tags> listaTags;
+		Usuario dono;	//usuario dono do post
+		Data data;
+		//std::list<usuarios> listaPermissoes; // lista de usuarios que podem ver o post
+		Permissao permissao;  //(PUBLIC, PRIVATE) utiliza a classe enum "Permissao"
+		std::vector<Comentario> comentarios;
+		std::list<Tags> listaTags;
 
 };
 
